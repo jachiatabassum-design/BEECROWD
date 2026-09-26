5de8c6531c938d6c60669797cf68fd420c2aab98
jachiatabassum-design <jachiatabassum@gmail.com>
2026-09-26 12:22:16 +0200

    Add files via upload

---
+++ BEE-1046.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int start , end ;
+    scanf("%d %d",&start , &end) ;
+    if (start>end)
+    {
+    printf("O JOGO DUROU %d HORA(S)\n",(24-start)+end) ;
+    }
+    else if(start<end)
+    {
+        printf("O JOGO DUROU %d HORA(S)\n",end-start) ;
+    }
+    else if(start == end)
+    {
+         printf("O JOGO DUROU %d HORA(S)\n",24) ;
+    }
+    return 0;
+}
